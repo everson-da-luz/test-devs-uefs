f5eadcd53538c2e0470a2385edabdd455a9aab50
Éverson da Luz <eversonmdaluz@gmail.com>
2025-11-27 14:26:59 -0300

    feat: CRUD de tags

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -2,9 +2,165 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Tag;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TagController extends Controller
 {
+    public function index(): JsonResponse
+    {
+        $modelTag = new Tag();
 
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => '',
+            'data' => $modelTag->getAll()
+        ], 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $dataPost = $request->post();
+        $validator = Validator::make($dataPost, Tag::rules('insert'), Tag::errorMessage('insert'));
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'code' => 422,
+                'message' => $validator->errors()->all(),
+                'data' => $dataPost
+            ], 422);
+        }
+
+        try {
+            $tagCreated = Tag::create($request->all());
+
+            if (! $tagCreated) {
+                throw new \Exception('Houve um erro ao tentar criar a tag.');
+            }
+
+            $response = [
+                'success' => true,
+                'code' => 200,
+                'message' => 'Tag criada com sucesso.',
+                'data' => $tagCreated
+            ];
+        } catch (\Exception $e) {
+            $response = [
+                'success' => false,
+                'code' => 500,
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+        }
+
+        return response()->json($response, $response['code']);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $modelTag = new Tag();
+        $tag = $modelTag->getById($id);
+
+        if (! $tag) {
+            $response = [
+                'success' => false,
+                'code' => 404,
+                'message' => 'Tag não encontrada.',
+                'data' => []
+            ];
+        } else {
+            $response = [
+                'success' => true,
+                'code' => 200,
+                'message' => '',
+                'data' => $tag
+            ];
+        }
+
+        return response()->json($response, $response['code']);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $dataRequest = $request->all();
+        $dataRequest['id'] = $id;
+        $validator = Validator::make($dataRequest, Tag::rules('update'), Tag::errorMessage('update'));
+
+        if (empty($dataRequest) || $validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'code' => 422,
+                'message' => $validator->errors()->all(),
+                'data' => $dataRequest
+            ], 422);
+        }
+
+        try {
+            $modelTag = new Tag();
+            $tagUpdated = $modelTag->updateTag($dataRequest);
+
+            if (! $tagUpdated) {
+                throw new \Exception('Houve um erro ao tentar atualizar a tag.');
+            }
+
+            $response = [
+                'success' => true,
+                'code' => 200,
+                'message' => 'Tag atualizada com sucesso.',
+                'data' => $tagUpdated
+            ];
+        } catch (\Exception $e) {
+            $response = [
+                'success' => false,
+                'code' => 500,
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+        }
+
+        return response()->json($response, $response['code']);
+    }
+
+    public function destroy(Request $request, $id): JsonResponse
+    {
+        $dataRequest = $request->all();
+        $dataRequest['id'] = $id;
+        $validator = Validator::make($dataRequest, Tag::rules('delete'), Tag::errorMessage('delete'));
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'code' => 422,
+                'message' => $validator->errors()->all(),
+                'data' => $dataRequest
+            ], 422);
+        }
+
+        try {
+            if (! Tag::destroy($id)) {
+                throw new \Exception('Houve um erro ao tentar excluir a tag.');
+            }
+
+            $response = [
+                'success' => true,
+                'code' => 200,
+                'message' => 'Tag excluida com sucesso.',
+                'data' => $dataRequest
+            ];
+        } catch (\Exception $e) {
+            $response = [
+                'success' => false,
+                'code' => 500,
+                'message' => $e->getMessage(),
+                'data' => []
+            ];
+        }
+
+        return response()->json($response, $response['code']);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Post;
 use App\Models\PostTag;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -34,5 +35,86 @@ class Tag extends Model
     public function posts_tags(): HasMany
     {
         return $this->hasMany(PostTag::class, 'tags_id');
+    }
+
+    public static function rules(string $context = 'insert'): array
+    {
+        $rules = [
+            'insert' => [
+                'name' => 'required'
+            ],
+            'update' => [
+                'id' => 'integer|exists:tags,id',
+                'name' => 'sometimes|required'
+            ],
+            'delete' => [
+                'id' => 'required|integer|exists:tags,id',
+            ]
+        ];
+
+        if (!array_key_exists($context, $rules)) {
+            return [];
+        }
+
+        return $rules[$context];
+    }
+
+    public static function errorMessage(string $context = 'insert'): array
+    {
+        $errorMessages = [
+            'insert' => [
+                'name' => [
+                    'required' => 'Nome obrigatório.'
+                ]
+            ],
+            'update' => [
+                'id' => [
+                    'required' => 'ID obrigatório.',
+                    'integer' => 'ID deve ser um número.',
+                    'exists' => 'Tag não encontrada.'
+                ],
+                'name' => [
+                    'required' => 'Nome obrigatório.'
+                ]
+            ],
+            'delete' => [
+                'id' => [
+                    'required' => 'ID obrigatório.',
+                    'integer' => 'ID deve ser um número.',
+                    'exists' => 'Tag não encontrada.'
+                ]
+            ]
+        ];
+
+        if (!array_key_exists($context, $errorMessages)) {
+            return [];
+        }
+
+        return $errorMessages[$context];
+    }
+
+    public function getAll():? Collection
+    {
+        return $this->all();
+    }
+
+    public function getById(int $id):? Tag
+    {        
+        return $this->find($id);
+    }
+
+    public function updateTag($data): bool
+    {
+        if (!isset($data['id']) || empty($data['id'])) {
+            return false;
+        }
+
+        $tag = self::find($data['id']);
+
+        if (empty($tag)) {
+            return false;
+        }
+
+        return $tag->update($data);
     }
 }
